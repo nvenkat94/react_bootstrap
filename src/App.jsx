2dfce0c51e770e56cb54adca981114423009d6bf
venkatesan <venkatesan.muthaiyan@crosshurdle.in>
2022-10-23 23:20:24 +0530

    Doubt

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import { Button } from 'react-bootstrap'
+// import Button from 'react-bootstrap/Button'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      <div style={{padding:'5rem'}}>
+        <Tabs
+          id="controlled-tab-example"
+          onSelect={(k) => setKey(k)}
+          className="mb-3"
+        >
+          <Tab eventKey="home" title="Home">
+            Home Sonnet
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+            Profile sonnet
+          </Tab>
+          <Tab eventKey="contact" title="Contact" disabled>
+            Contact sonnet
+          </Tab>
+        </Tabs></div>
     </div>
   )
 }
